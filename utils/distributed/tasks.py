70b02db71a905aa755724efdf503b62eeb0e077a
Stephen DiCato <sdicato@mitre.org>
2013-05-28 13:48:00 -0400

    Actually submit a task to cuckoo

--- utils/distributed/tasks.py
+++ utils/distributed/tasks.py
@@ -11,10 +11,15 @@ from celery import Celery
 
 from lib.cuckoo.common.config import Config
 from lib.cuckoo.common.constants import CUCKOO_ROOT
+from lib.cuckoo.core.database import Database
+from lib.cuckoo.common.utils import store_temp_file
 
 
+# Local Cuckoo config and database
 config = Config(os.path.join(CUCKOO_ROOT, "conf", "cuckoo.conf"))
+db = Database()
 
+# Our Cuckoo-Celery App
 celery = Celery('cuckoo',
                 broker=config.celery.broker,
                 backend=config.celery.backend,
@@ -29,7 +34,11 @@ def submit_sample(filename, data):
     print "My hostname: %s" % submit_sample.request.hostname
     print "Received a file: %s" % filename
     print "File size: %s" % len(data)
-    return submit_sample.request.hostname + ':' + str(random.randint(0, 1000))
+
+    temp_file_path = store_temp_file(data, filename)
+    task_id = db.add_path(file_path=temp_file_path)
+
+    return (submit_sample.request.hostname, task_id)
 
 
 @celery.task
