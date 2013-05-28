9e9a3b7275f5dabb7c15f98e7191094db0543ea1
Stephen DiCato <sdicato@mitre.org>
2013-05-28 14:04:43 -0400

    Add submit support to distributed web utility.

--- utils/distributed/web.py
+++ utils/distributed/web.py
@@ -1,3 +1,113 @@
-"""
-Adaptation of utils/web.py that uses Celery.
-"""
+#!/usr/bin/env python
+
+import os
+import sys
+import logging
+import argparse
+try:
+    from jinja2.loaders import FileSystemLoader
+    from jinja2.environment import Environment
+except ImportError:
+    sys.stderr.write("ERROR: Jinja2 library is missing")
+    sys.exit(1)
+try:
+    from bottle import route, run, static_file, redirect, request, HTTPError, hook, response
+except ImportError:
+    sys.stderr.write("ERROR: Bottle library is missing")
+    sys.exit(1)
+
+logging.basicConfig()
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", ".."))
+
+from lib.cuckoo.common.constants import CUCKOO_ROOT
+from lib.cuckoo.common.utils import store_temp_file
+
+# Templating engine.
+env = Environment()
+env.loader = FileSystemLoader(os.path.join(CUCKOO_ROOT, "data", "html"))
+
+from utils.distributed import tasks
+
+
+@hook("after_request")
+def custom_headers():
+    """Set some custom headers across all HTTP responses."""
+    response.headers["Server"] = "Machete Server"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Cache-Control"] = "no-cache"
+    response.headers["Expires"] = "0"
+
+
+@route("/")
+def index():
+    context = {}
+    template = env.get_template("submit.html")
+    return template.render({"context" : context})
+
+
+@route("/browse")
+def browse():
+    pass
+
+
+@route("/static/<filename:path>")
+def server_static(filename):
+    return static_file(filename, root=os.path.join(CUCKOO_ROOT, "data", "html"))
+
+
+@route("/submit", method="POST")
+def submit():
+    context = {}
+    errors = False
+
+    package  = request.forms.get("package", "")
+    options  = request.forms.get("options", "")
+    priority = request.forms.get("priority", 1)
+    timeout  = request.forms.get("timeout", "")
+    data = request.files.file
+
+    try:
+        priority = int(priority)
+    except ValueError:
+        context["error_toggle"] = True
+        context["error_priority"] = "Needs to be a number"
+        errors = True
+
+    if data == None or data == "":
+        context["error_toggle"] = True
+        context["error_file"] = "Mandatory"
+        errors = True
+
+    if errors:
+        template = env.get_template("submit.html")
+        return template.render({"timeout" : timeout,
+                                "priority" : priority,
+                                "options" : options,
+                                "package" : package,
+                                "context" : context})
+
+
+    r = tasks.submit_sample.delay(data.filename, data.file.read())
+    # This is blocking!
+    task_id = r.get()
+
+    template = env.get_template("success.html")
+    return template.render({"taskid" : task_id,
+                            "submitfile" : data.filename.decode("utf-8")})
+
+
+@route("/view/<task_id>")
+def view(task_id):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-H", "--host", help="Host to bind the web server on", default="0.0.0.0", action="store", required=False)
+    parser.add_argument("-p", "--port", help="Port to bind the web server on", default=8080, action="store", required=False)
+    args = parser.parse_args()
+
+    run(host=args.host, port=args.port, reloader=True)
