492c217054d7a8961fc05f7b6e3d2c2d4e6fe22e
Ben <x@d-str.cc>
2014-07-09 20:34:36 +0000

    update to reflect bugfix in my branch

--- utils/cuckoo-tests/weaver.py
+++ utils/cuckoo-tests/weaver.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 import requests
 import sys
 import os
@@ -8,7 +8,7 @@ import time
 import argparse
 
 # REF cuckoo API endpoints
-status = "/cuckoo/status"
+statcheck = "/cuckoo/status"
 taskcheck = "/tasks/view/"
 tasklist = "/tasks/list"
 submit = "/tasks/create/file"
@@ -27,9 +27,11 @@ args = parser.parse_args()
 # check that cuckoo is up
 cuckoo_url = args.cuckoo
 
-rqst = requests.get(cuckoo_url + status)
-if (rqst.status_code != 200):
-    print "ERR: cuckoo is down"
+try:
+    rqst = requests.get(cuckoo_url + statcheck)
+
+except:
+    print "ERR: cuckoo server at " + cuckoo_url + " appears to be unresponsive - exiting"
     sys.exit(1)
 
 
@@ -37,49 +39,53 @@ if (rqst.status_code != 200):
 config_file = args.config
 
 config = ConfigParser.RawConfigParser()
-try:
-    config.read(config_file)
-except:
+
+if not config.read(config_file) :
     print "ERR: Configuration file did not exist: " + config_file
     exit(1)
+
 
 # Submit each sample and wait for results
 
 for sample_name in config.sections():
 
     # try to find sample under current dir
+    found = False
     for root,d,filename in os.walk("."):
         if sample_name in filename:
-            try:
-                loc = os.path.join(root, sample_name)
-            except NameError:
-                loc = "none"
-                
-        config.set(sample_name, "Outcome", "sample did not exist")
-        continue # skip to next sample
+            loc = os.path.join(root, sample_name)
+            found = True
+            break
+        if not found:
+            print "ERR sample " + sample_name + "did not exist"
+            config.set(sample_name, "Outcome", "sample did not exist")
+            continue # skip to next sample
 
     # submit sample to cuckoo 
     multi_part = {'file': open(loc, 'rb')}
     rqst = requests.post(cuckoo_url + submit, files=multi_part)
-    resp = rqst.json()
 
-    taskid = int(resp['task_id'])
+    try:
+        resp = rqst.json()
+        taskid = int(resp['task_id'])
+        status = "submitted"
+        if args.verbose:
+            print sample_name + " was submitted with task ID " + str(taskid)
+    except:
+        config.set(sample_name, "Outcome", "error in cuckoo response")
+        continue # skip to next sample
 
     # save off submission info
     config.set(sample_name, 'Location', loc)
     config.set(sample_name, 'Task', taskid)
 
-    if args.verbose:
-        print sample_name + " was submitted with task ID " + str(taskid)
-
-
     # spin until done processing sample
     timeout = 600
     spent = 0
-    while status != "reported" and spent < timeout:
+    while status != "reported" and spent < timeout :
         wait = 60
         if args.verbose:
-            print "waiting " + wait + " seconds for sample to process"
+            print "waiting " + str(wait) + " seconds for sample to process"
             
         time.sleep(wait)
         spent = spent + wait
@@ -89,7 +95,7 @@ for sample_name in config.sections():
         status = rqst.json()['task']['status']
         config.set(sample_name, 'Status', status)
         if args.verbose:
-                print "at time " + spent + " status of " + sample_name + " is " + status
+                print "at time " + str(spent) + " status of " + sample_name + " is " + status
     
     # we're done waiting, let's try to check the report        
     if (status != "reported"):
@@ -98,42 +104,57 @@ for sample_name in config.sections():
 
     else:
         # get report from cuckoo
-        
         rqst = requests.get(cuckoo_url + report + str(taskid))
         report = rqst.json()
-        category = config.get(sample_name, 'Behavior')
-        ind = config.get(sample_name, 'Indicator')
     
-        # check for  indicators
+        # collect indicators from config
+        detections = zip(config.get(sample_name, 'Section').split(','), config.get(sample_name, 'Indicator').split(','))
 
-        if args.verbose:
-            print "checking for "+ ind + " under " + category
+        found = False
+        for i, (section, ind) in enumerate(detections):
+            if args.verbose:
+                print "checking " + sample_name + " for "+ ind + " under " +section
 
-        result =  "report FAILED"                
-        # find network indicators
-        if "network" in category:
-            try:
-                # i.e. if indicator is "http", we have a non-empty list in report[network][http]
-                if report['network'][ind]:
-                        result =  "report PASSED"
-            except KeyError:
-                print "ERR invalid config of " + category 
+            # - find network indicators
+            if "network" in section:
+                try:
+                    # i.e. if indicator is "http", we have a non-empty list in report[network][http]
+                    if report['network'][ind]:
+                        found=  True
+                    else:
+                        found = False
+                except KeyError:
+                    print "ERR invalid config value " +section 
+                
+            else:
+            # - examine host indicators
+                try:
+                    # i.e. report[behavior][summary][mutexes] contains "evilmutex"
+                    for item in report['behavior']['summary'][section]:
+                        if ind in item:
+                            found=  True
+                            break
+                        else:
+                            found = False
+                except KeyError:
+                    print "ERR invalid config value " +section 
             
-        else:
+            if args.verbose:
+                print "Were indicators for " + sample_name + " in report? " + str(found)
             
-        # find host indicators
-            try:
-                # i.e. report[behavior][summary][mutexes] contains "evilmutex"
-                for item in report['behavior']['summary'][category]:
-                    if ind in item:
-                        result =  "report PASSED"
-            except KeyError:
-                print "ERR invalid config of " + category 
-        
-        if args.verbose:
-            print sample_name + " had an outcome of " + outcome
-            
-        config.set(sample_name, "Outcome", result)
+            # short-circuit if one indicator is not found 
+            # allows AND behavior for multiple indicators
+            if found == False:
+                break
+
+        # end for
+                
+        # log result
+        if found: 
+            config.set(sample_name, "Outcome", "SUCCESS")
+        else: 
+            config.set(sample_name, "Outcome", "report did not contain all specified indicators")
+
 
 # done processing,all samples, write output file
 outfile = config_file + ".result"
